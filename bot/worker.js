b108f2e7ed28d8db1c2afd2b1557eafb9acf2680
z0374 <33524313+z0374@users.noreply.github.com>
2025-04-02 14:41:34 -0300

    teste dataBase

--- bot/worker.js
+++ bot/worker.js
@@ -322,10 +322,10 @@ if (tableExists.results.length === 0) {  // Verifica se a tabela existe
             ;
 
             // Envia as informações para o envio colunas
-            await sendMessage(`${valores} - ${tabela[1]}\\n - ${content}\n\n${query}`, env);
+            await sendMessage(`${valores} - ${tabela[1]}\n - ${content}\n\n${query}`, env);
 
             // Executa a inserção dos dados usando os valores fornecidos
-            await _data.prepare(query).run(...content); // Usa `content` para passar os dados para os placeholders
+            await _data.prepare(query).bind(content).run(); // Usa `content` para passar os dados para os placeholders
 
             const sucesso = 'Salvo com sucesso!';
             await sendMessage(sucesso, env); // Envia a mensagem de sucesso para o usuário
