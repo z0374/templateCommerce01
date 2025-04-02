72f04eb1ff813f790134948f5fed3355fe6503cb
z0374 <33524313+z0374@users.noreply.github.com>
2025-04-02 14:31:32 -0300

    teste dataBase

--- bot/worker.js
+++ bot/worker.js
@@ -318,10 +318,8 @@ if (tableExists.results.length === 0) {  // Verifica se a tabela existe
                       return new Response(mensagem, { status: 400 }); // Retorna resposta com erro
                     }
             // Cria a consulta SQL para inserir os dados
-            const query = `
-              INSERT INTO "${tabela[0]}" (${tabela[1]})
-              VALUES (${valores});
-            `;
+            const query = "INSERT INTO " + tabela[0] + " (" + tabela[1] + ") VALUES (" + valores + ")";
+            ;
 
             // Envia as informações para o envio colunas
             await sendMessage(`${valores} - ${tabela[1]}\\n - ${content}\n\n${query}`, env);
