b20a64075688089fce41232865cd8fa0f0764db0
z0374 <33524313+z0374@users.noreply.github.com>
2025-04-04 16:06:03 -0300

    repair sintax

--- bot/worker.js
+++ bot/worker.js
@@ -162,9 +162,9 @@ await processos(messageText);
                       const dataLogo = downloadGdrive(logoId, env);
                       const dataName = await dados('read',dataId[1],'assets',userId)['nome'];
                       const dataAcss = await dados('read',dataId[2],'assets',userId)['nome'];
-                      let databtn ='';
+                      let databtn;
                           for(let i=0;i<dataId[3].length;i++){
-                            const data3 = await dados('read',dataId[3],'assets',userId)['nome'];
+                            const data3 = await dados('read',dataId[3][i],'assets',userId)['nome'];
                             databtn += `${i+1} - Rótulo: ${data3[0]} - URL: ${data3[1]}\n`;
                           }
 
